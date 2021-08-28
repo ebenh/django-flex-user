1d58d8bcdae18f82f5a5429772760fd4fa4f180d
Ebenezer Hailemariam <eben@derso.org>
2021-08-28 12:07:56 -0400

    Added "django_flex_user" to test_project

--- test_project/settings.py
+++ test_project/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -31,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_flex_user.apps.DjangoFlexUserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
