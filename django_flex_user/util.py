4f3d233579801883761f5e1658db23a9dafc14b1
Ebenezer Hailemariam <eben@derso.org>
2021-09-04 18:40:12 -0400

    Added base case handling

--- django_flex_user/util.py
+++ django_flex_user/util.py
@@ -47,5 +47,8 @@ def obscure_email(email):
 
 
 def obscure_phone(phone):
+    if not phone:
+        return ''
+
     count = sum(c.isdigit() for c in phone)
     return re.sub(r'\d', '*', phone, count=count - 2)
