11ecd1cbe94efdca96c209a0df1d03eea362ecd5
Ebenezer Hailemariam <eben@derso.org>
2021-09-04 10:58:38 -0400

    Added verbose_name to app

--- django_flex_user/apps.py
+++ django_flex_user/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DjangoFlexUserConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'django_flex_user'
+    verbose_name = "Django Flex User"
