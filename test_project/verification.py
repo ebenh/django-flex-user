cc47f8d3fee99cdbebc9ecc3d238c8506d94f0bf
Ebenezer Hailemariam <eben@derso.org>
2021-09-16 11:18:33 -0400

    We're now sending actual emails for otp delivery

--- test_project/verification.py
+++ test_project/verification.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
+
+
 def email_otp(recipient, challenge):
-    print(recipient)
-    print(challenge)
+    send_mail('Verify your account', f'Your one-time password:\n\n{challenge}', 'eben@derso.org', (recipient,))
 
 
 def sms_otp(recipient, challenge):
