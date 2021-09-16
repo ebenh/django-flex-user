42501ece1b0eac6c12679979469a3561d520b576
Ebenezer Hailemariam <eben@derso.org>
2021-09-16 19:15:02 -0400

    Updated tests

--- django_flex_user/tests/otp/test_endpoint_otp_devices.py
+++ django_flex_user/tests/otp/test_endpoint_otp_devices.py
@@ -12,91 +12,47 @@ class TestOTPDevicesRetrieve(APITestCase):
         response = self.client.get(self._REST_ENDPOINT_PATH)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_method_get_search_non_existent_user(self):
-        response = self.client.get(self._REST_ENDPOINT_PATH, {'search': 'validUsername'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            response.data,
-            {
-                'EmailDevice': [],
-                'PhoneDevice': []
-            }
-        )
-
-    def test_method_get_search_existing_user_by_username(self):
+    def test_method_get_no_matches(self):
         from django_flex_user.models import FlexUser
 
         FlexUser.objects.create_user(username='validUsername', email='validEmail@example.com', phone='+12025551234')
 
-        response = self.client.get(self._REST_ENDPOINT_PATH, {'search': 'validUsername'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            response.data,
-            {
-                'EmailDevice': [
-                    {
-                        'id': 1,
-                        'name': 'va********@ex*****.***'
-                    }
-                ],
-                'PhoneDevice': [
-                    {
-                        'id': 1,
-                        'name': '+*********34'
-                    }
-                ]
-            }
-        )
+        for q in ('', 'validUsername2', 'validEmail2@example.com', '+12025554321', 'valid', '+1202'):
+            response = self.client.get(self._REST_ENDPOINT_PATH, {'search': q})
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(
+                response.data,
+                {
+                    'EmailDevice': [],
+                    'PhoneDevice': []
+                }
+            )
 
-    def test_method_get_search_existing_user_by_email(self):
+    def test_method_get_match_found(self):
         from django_flex_user.models import FlexUser
 
         FlexUser.objects.create_user(username='validUsername', email='validEmail@example.com', phone='+12025551234')
 
-        response = self.client.get(self._REST_ENDPOINT_PATH, {'search': 'validEmail@example.com'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            response.data,
-            {
-                'EmailDevice': [
-                    {
-                        'id': 1,
-                        'name': 'va********@ex*****.***'
-                    }
-                ],
-                'PhoneDevice': [
-                    {
-                        'id': 1,
-                        'name': '+*********34'
-                    }
-                ]
-            }
-        )
-
-    def test_method_get_search_existing_user_by_phone(self):
-        from django_flex_user.models import FlexUser
-
-        FlexUser.objects.create_user(username='validUsername', email='validEmail@example.com', phone='+12025551234')
-
-        response = self.client.get(self._REST_ENDPOINT_PATH, {'search': '+12025551234'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(
-            response.data,
-            {
-                'EmailDevice': [
-                    {
-                        'id': 1,
-                        'name': 'va********@ex*****.***'
-                    }
-                ],
-                'PhoneDevice': [
-                    {
-                        'id': 1,
-                        'name': '+*********34'
-                    }
-                ]
-            }
-        )
+        for q in ('validUsername', 'validEmail@example.com', '+12025551234'):
+            response = self.client.get(self._REST_ENDPOINT_PATH, {'search': q})
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            self.assertEqual(
+                response.data,
+                {
+                    'EmailDevice': [
+                        {
+                            'id': 1,
+                            'name': 'va********@ex*****.***'
+                        }
+                    ],
+                    'PhoneDevice': [
+                        {
+                            'id': 1,
+                            'name': '+*********34'
+                        }
+                    ]
+                }
+            )
 
     def test_method_post(self):
         response = self.client.post(self._REST_ENDPOINT_PATH)
