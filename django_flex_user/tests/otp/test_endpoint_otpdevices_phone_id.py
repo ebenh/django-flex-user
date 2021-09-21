1aaa7948a9041cd10cfd8ed7f015072c0210ebd7
Ebenezer Hailemariam <eben@derso.org>
2021-09-21 15:28:44 -0400

    Adding new test file

---
+++ django_flex_user/tests/otp/test_endpoint_otpdevices_phone_id.py
@@ -0,0 +1,14 @@
+from .test_endpoint_otpdevices_email_id import TestEmailDeviceUpdate
+
+
+class TestPhoneDeviceUpdate(TestEmailDeviceUpdate):
+    def setUp(self):
+        from django_flex_user.models import FlexUser
+        from django_flex_user.models import PhoneDevice
+
+        user = FlexUser.objects.create_user(username='validUsername',
+                                            email='validEmail@example.com',
+                                            phone='+12025551234')
+        self.otp_device = PhoneDevice.objects.get(user=user)
+
+        self._REST_ENDPOINT_PATH = TestEmailDeviceUpdate._REST_ENDPOINT_PATH.format(type='phone', id=self.otp_device.id)
