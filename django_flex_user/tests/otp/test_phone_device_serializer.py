200f10151c5d27586a197b9a46d112e4fca16f0c
Ebenezer Hailemariam <eben@derso.org>
2021-09-22 19:03:18 -0400

    Adding new test file

---
+++ django_flex_user/tests/otp/test_phone_device_serializer.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+
+
+class TestPhoneDeviceSerializer(TestCase):
+    """
+    This class is designed to test django_flex_user.serializers.PhoneDeviceSerializer
+    """
+
+    def setUp(self):
+        from django_flex_user.models import FlexUser
+
+        self.user1 = FlexUser.objects.create_user(username='validUsername1',
+                                                  email='validEmail1@example.com',
+                                                  phone='+12025550001')
+        self.user2 = FlexUser.objects.create_user(username='validUsername2',
+                                                  email='validEmail2@example.com',
+                                                  phone='+12025550002')
+
+    def test_serialize(self):
+        from django_flex_user.models.otp import PhoneDevice
+        from django_flex_user.serializers import EmailDeviceSerializer
+
+        email_device = PhoneDevice.objects.get(user=self.user1)
+
+        # Make sure the serializer only exposes the data we want it to
+        serializer = EmailDeviceSerializer(email_device)
+        self.assertEqual(
+            serializer.data,
+            {
+                'id': 1,
+                'name': '+*********01'
+            }
+        )
+
+    def test_serialize_many(self):
+        from collections import OrderedDict
+        from django_flex_user.models.otp import PhoneDevice
+        from django_flex_user.serializers import PhoneDeviceSerializer
+
+        email_devices = PhoneDevice.objects.all()
+
+        # Make sure the serializer only exposes the data we want it to
+        serializer = PhoneDeviceSerializer(email_devices, many=True)
+        print(serializer.data)
+        self.assertEqual(
+            serializer.data,
+            [
+                OrderedDict([('id', 1), ('name', '+*********01')]),
+                OrderedDict([('id', 2), ('name', '+*********02')])
+            ]
+        )
