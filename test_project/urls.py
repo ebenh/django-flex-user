657d646815f74e95d847c01177af6f6e475b0360
Ebenezer Hailemariam <eben@derso.org>
2021-09-28 18:06:17 -0400

    Add password reset link to sign in page

--- test_project/urls.py
+++ test_project/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('accounts/sign-up/', views.sign_up, name='sign-up'),
     path('accounts/sign-in/', views.sign_in, name='sign-in'),
     path('accounts/sign-out/', views.sign_out, name='sign-out'),
-    path('accounts/password-reset/find-my-account/', views.search_otp_tokens),
+    path('accounts/password-reset/find-my-account/', views.search_otp_tokens, name='password-reset'),
     path('accounts/password-reset/verify/<slug:token_type>/<int:token_id>/', views.verify_otp),
     path('accounts/password-reset/change-password/', views.change_password),
 
