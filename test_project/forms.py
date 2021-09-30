97f975f5806c4b34151e087709b0844e8a042fc6
Ebenezer Hailemariam <eben@derso.org>
2021-09-30 19:49:05 -0400

    Updated text displayed to the user for better UX

--- test_project/forms.py
+++ test_project/forms.py
@@ -36,7 +36,7 @@ class VerifyOTPForm(forms.Form):
             raise ValidationError("You're doing that too much. Please wait before trying again.")
         else:
             if not success:
-                raise ValidationError('The password you entered was incorrect.')
+                raise ValidationError('The verification code you entered was incorrect.')
 
         return password
 
