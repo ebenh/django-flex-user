bcf514b447952a897ca5b78bc3573dbb76280e6c
Ebenezer Hailemariam <eben@derso.org>
2021-10-06 09:20:28 -0400

    Updated settings file

--- test_project/settings.py
+++ test_project/settings.py
@@ -2,11 +2,11 @@ from split_settings.tools import optional, include
 
 include(
     'components/settings.py',
-    'components/django_flex_user.py',
     'components/django_phonenumber_field.py',
-    'components/social_auth_app_django.py',
-    'components/sendgrid.py',
+    'components/django_flex_user.py',
     'components/djangorestframework.py',
     'components/drf_multiple_model.py',
+    'components/social_auth_app_django.py',
+    'components/sendgrid.py',
     optional('local_settings.py')
 )
